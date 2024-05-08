87332ae94519dbf5839f9aca269739a275359248
Devrim Akbulut <Devrimakb1@gmail.com>
2024-05-08 15:08:34 +0200

    Footer done, need to change bg color, could change link texts

--- app/(root)/_components/footer.jsx
+++ app/(root)/_components/footer.jsx
@@ -3,33 +3,34 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-between bg-gray-500 fixed bottom-0 w-full">
+    
+    <div className="flex justify-between p-2 bg-gray-500 fixed bottom-0 w-full">
         <div className="p-2">
-            <p>LOGO</p>
+            <p className="">LOGO</p>
         </div>
-        <div className="flex gap-8 m-auto text-center">
-        <div className="">
-            <ul>
-                <li>Career</li>
+        <div className="flex gap-24 m-auto text-center">
+        <div>
+            <p className="text-lg ">Career</p>
+            <ul className="text-sm">
                 <Link href="/"><li>Avaliable jobs</li></Link>
                 <Link href="/"><li>Our positions</li></Link>
                 <Link href="/"><li>Work with us!</li></Link>
             </ul>
         </div>
-        <div className="">
-            <ul>
-            <li>Terms of service</li>
-            <Link href="/"><li>FAQ</li></Link>
+        <div>
+            <p className="text-lg">Terms of service</p>
+            <ul className="text-sm">
             <Link href="/"><li>Terms</li></Link>
+            <Link href="/"><li>FAQ</li></Link>
             </ul>
         </div>
-        <div className="">
-            <ul>
-                <li>The company</li>
-                <Link href="/"><li>Employees</li></Link>
-                <Link href="/"><li>Contact</li></Link>
-                <Link href="/"><li>Hej</li></Link>
-            </ul>
+        <div>
+          <p className="text-lg">The company</p>
+          <ul className="text-sm">   
+              <Link href="/"><li>Employees</li></Link>
+              <Link href="/"><li>Contact</li></Link>
+              <Link href="/"><li>Hej</li></Link>
+          </ul>
         </div>
     </div>
     </div>
