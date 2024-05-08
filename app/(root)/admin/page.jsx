344753801e9afa80bf5ef4ea19631f5843f01484
Felicia Hu <feliciahu1992_@hotmail.com>
2024-05-09 01:20:12 +0200

    update admin page

--- app/(root)/admin/page.jsx
+++ app/(root)/admin/page.jsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { useUser } from "@clerk/clerk-react";
+import { useRouter } from "next/navigation";
+
 import EventsList from "./_components/EventsList"
 
 const AdminPage = () => {
+  const { isSignedIn, user, isLoading } = useUser();
+  const router = useRouter();
+ 
+  if (isLoading) return;
+  if (!isSignedIn) router.push('/sign-in');
+  console.log(user)
   return (
     <>
-    <div>My Dashboard</div>
-    <EventsList />
+    <div className="mt-40 flex justify-center">My Dashboard</div>
+    <div className="mt-40 flex justify-center">
+      <EventsList />
+    </div>
+    
+    
     </>
   )
 }
