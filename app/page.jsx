1ece5f1808dbb4fd7cf0a7ac26d20116742681cd
cellrogue <ella.burnas@gmail.com>
2024-05-08 15:19:10 +0200

    added more front-text

--- app/page.jsx
+++ app/page.jsx
@@ -8,12 +8,13 @@ const LandingPage = () => {
       <div className="flex justify-center flex-col items-center w-full p-2 mt-8">
         <div className="">
           <h1>Welcome to (insert name here)</h1>
-          <p>Check out our current events!</p>
+          <p>We display all the current events happening around Stockholm. See anything interesting? Don't forget to sign up for the event in time!</p>
+            <div className="flex justify-center items center">
+              <img src={img} alt="" />
+            </div>
+          <p className="flex text-xl mt-20 justify-center items-center">Check out the current events!</p>
         </div>
-        <div className="front-image">
-          <img src={img} alt="" />
-        </div>
-        <div className="border p-2 rounded border-green-400 flex gap-4 mt-10">
+        <div className="border p-2 rounded border-green-400 flex gap-4 mt-5">
           {list.map((item, i) => {
             return <EventComponent name={item.name} img={item.img}/>
           })}
