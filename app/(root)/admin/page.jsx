cb7a5980f7ffb25f41c7c24d22cb34af521be312
Emma Mellgren <mellgrenemma0@gmail.com>
2024-05-10 17:57:35 +0200

    Update EventsList, dashboard & global values

--- app/(root)/admin/page.jsx
+++ app/(root)/admin/page.jsx
@@ -11,14 +11,15 @@ const AdminPage = () => {
     return (
         <>
             <Navbar />
-            <header className='bg-primary shadow-md shadow-secondary'>
+            <header className='bg-primary shadow'>
                 <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
-                    <h1 className='text-3xl font-bold tracking-tight text-gray-900'>
+                    <h1 className='text-3xl font-bold tracking-tight text-tertiary'>
                         Dashboard
                     </h1>
                 </div>
             </header>
-            <main className='mt-40 flex justify-center'>
+            <main className='my-10 flex flex-col justify-center'>
+              <h2 className="flex max-w-7xl px-4 py-6 sm:px-6 lg:px-8">All events</h2>
                 <EventsList />
             </main>
         </>
