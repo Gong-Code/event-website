400b2c4db2e6ab66954b7677ab4b416d97fcf75b
cellrogue <ella.burnas@gmail.com>
2024-05-12 13:58:02 +0200

    added get by id function

--- app/api/events/[id]/route.js
+++ app/api/events/[id]/route.js
@@ -1,3 +1,9 @@
+const { db } = require("@/firebase.config")
+
 //GET BY ID, hämta en event via ID 
+export const fetchDataByID = (collection, id) => {
+    return db.collection(collection).doc(id).get()
+}
+
 //PATCH by ID
 //DELETE BY ID
