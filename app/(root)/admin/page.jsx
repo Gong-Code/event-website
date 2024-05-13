289d0ef166ecf3a3400aed7e3f32fd9dce8b8ab4
Emma Mellgren <mellgrenemma0@gmail.com>
2024-05-13 17:43:51 +0200

    upd navbar

--- app/(root)/admin/page.jsx
+++ app/(root)/admin/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Navbar from '../_components/navbar';
 import { EventsList } from './_components/EventsList';
 
 import { FaPlus } from "react-icons/fa";
@@ -8,7 +7,6 @@ import { FaPlus } from "react-icons/fa";
 const AdminPage = () => {
     return (
         <>
-            <Navbar />
             <header className='bg-primary shadow'>
                 <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
                     <h1 className='text-3xl font-bold tracking-tight text-tertiary'>
