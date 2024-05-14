596ff2a1a8580bff8e54e866f3dba80db15ab78e
Emma Mellgren <mellgrenemma0@gmail.com>
2024-05-14 10:45:56 +0200

    Updated auth layout

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,21 +1,6 @@
 'use client'
 
-
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useAuth } from "../(root)/admin/_components/auth-provider";
-
-
 export default function AuthLayout({ children }) {
-    const { user, authLoaded } = useAuth;
-    const router = useRouter();
-
-    useEffect(() => {
-        if (authLoaded && !user) {
-            router.push('/');
-        } 
-    }, [user, authLoaded, router]);
-
   
     return (
         <div className="flex items-center justify-center">
