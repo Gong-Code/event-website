e45dd1834650b23d1a88acd21cf91240c77075f4
cellrogue <ella.burnas@gmail.com>
2024-05-14 16:09:23 +0200

    added filter-function

--- app/page.jsx
+++ app/page.jsx
@@ -1,51 +1,63 @@
+'use client'
+
 import Image from 'next/image';
 import { Event } from './(root)/_components/event';
+import { useState } from 'react';
+
+const img = '/assets/placeholder.jpg';
+const list = [
+    {
+        name: 'International Soup day',
+        img,
+        address: 'Earth',
+        date: '24-05-22 17.00',
+        numberOfPlaces: 15,
+        users: ['dinomon'],
+        id: 'abc',
+        maxUsers: 15
+    },
+    {
+        name: 'Opera in Hagaparken',
+        img,
+        address: 'Earth',
+        date: '24-06-23 17.00',
+        numberOfPlaces: 15,
+        users: ['dinomon'],
+        id: 'def',
+        maxUsers: 30
+    },
+    {
+        name: 'Våffeldagen',
+        img,
+        address: 'Earth',
+        date: '24-07-25 17.00',
+        numberOfPlaces: 5,
+        users: ['dinomon'],
+        id: 'ghi',
+        maxUsers: 25
+    },
+];
 
 const LandingPage = () => {
-    const img = '/assets/placeholder.jpg';
-    const eventList = [
-        {
-            name: 'International Soup day',
-            img,
-            address: 'Earth',
-            date: '24-05-22 17.00',
-            numberOfPlaces: 15,
-            users: ['dinomon'],
-            id: 'abc',
-            maxUsers: 15
-        },
-        {
-            name: 'Opera in Hagaparken',
-            img,
-            address: 'Earth',
-            date: '24-06-23 17.00',
-            numberOfPlaces: 15,
-            users: ['dinomon'],
-            id: 'def',
-            maxUsers: 30
-        },
-        {
-            name: 'Våffeldagen',
-            img,
-            address: 'Earth',
-            date: '24-07-25 17.00',
-            numberOfPlaces: 5,
-            users: ['dinomon'],
-            id: 'ghi',
-            maxUsers: 25
-        },
-    ];
+    const [searchValue, setSearchValue] = useState('')
+    const [eventList, setEventList] = useState(list)
+
+    const onSearch = (e) => {
+        setSearchValue(e.target.value)
+        const newList = list.filter(x => x.name.toLowerCase().includes(e.target.value.toLowerCase()))
+        setEventList(newList)
+    }
 
     return (
         // Alva la till py-32 för att anpassa sidan till navbaren.
         <div className='flex py-32 justify-center flex-col items-center w-full p-2 mt-8'>
             <div>
-                <h1>Welcome to (insert name here)</h1>
-                <p>
+                <h1 className='text-tertiary'>Welcome to (insert name here)</h1>
+                <p className='text-base'>
                     We display all the current events happening around
                     Stockholm. See anything interesting? Do not forget to sign up for the event in time!
                 </p>
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center items-center mt-8'>
                     <Image
                         src={img}
                         width={500}
@@ -57,7 +69,8 @@ const LandingPage = () => {
                     Check out the current events!
                 </p>
             </div>
-            <div className='border p-2 rounded border-green-400 flex gap-4 mt-5'>
+            <input type="text" value={searchValue} onChange={onSearch} className='text-black' placeholder='Search for an event...' />
+            <div className='p-2 flex gap-4 mt-5'>
                 {eventList.map((item, i) => {
                     return (
                         <Event
