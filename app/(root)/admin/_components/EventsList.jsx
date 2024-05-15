596d5131bc8cb456b7ce30c8caf3e39a599c1407
Emma Mellgren <mellgrenemma0@gmail.com>
2024-05-15 12:22:23 +0200

    Updated styling for events

--- app/(root)/admin/_components/EventsList.jsx
+++ app/(root)/admin/_components/EventsList.jsx
@@ -1,70 +1,90 @@
-'use client'
+'use client';
 
 import { getAllEvents } from '@/app/api/events/route';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { GoArrowRight } from "react-icons/go";
+import { GoArrowRight } from 'react-icons/go';
 
 export const EventsList = () => {
+    const [events, setEvents] = useState([]);
 
-const [events, setEvents] = useState([])
+    // FETCH EVENTS
+    const fetchEvents = async () => {
+        const fetchedEvents = await getAllEvents();
+        setEvents(fetchedEvents);
+    };
 
-// FETCH EVENTS
-const fetchEvents = async () => {
-    const fetchedEvents = await getAllEvents();
-    setEvents(fetchedEvents);
-};
+    // DISPLAY EVENTS
+    useEffect(() => {
+        fetchEvents();
+    }, []);
 
-// DISPLAY EVENTS
-useEffect(() => {
-    fetchEvents();
-}, []);
-
-console.log(events)
+    console.log(events);
 
     return (
-        <div className='relative bg-primary rounded-3xl py-4 px-12'>
-            <ul
-                role='list'
-                className='divide-y divide-gray-300 mx-auto'>
-                {events && events.length ? events.map((event) => (
-                    <li
-                        key={event.date}
-                        className='flex mx-auto justify-between max-w-7xl items-center'>
-                        <div className='flex items-center gap-6'>
-                            <Image
-                                className='h-12 w-12 rounded-full bg-gray-50'
-                                src={event.imageUrl}
-                                alt=''
-                                width={500}
-                                height={500}
-                            />
-                            <div className='min-w-0 flex-auto'>
-                                <p className='text-sm font-bold text-gray-900 border-b-2 border-b-tertiary'>
-                                    {event.name}
-                                </p>
-                                <div className='flex gap-2 items-center my-3'>
-                                    <span className='font-semibold text-gray-900 text-xs'>
-                                        Date:{' '}
-                                    </span>
-                                    <span className='text-xs text-gray-500'>
+        <>
+            <div className='bg-primary rounded-xl border-b border-gray-900/10 shadow-sm'>
+                <table className='text-slate-900 w-full table-auto'>
+                    <thead className='border-b border-gray-900/10 pb-12'>
+                        <tr>
+                            <th></th>
+                            <th className='p-4 text-left font-semibold text-sm'>
+                                Event name
+                            </th>
+                            <th className='p-4 text-left font-semibold text-sm'>
+                                Location
+                            </th>
+                            <th className='p-4 text-left font-semibold text-sm'>
+                                Date
+                            </th>
+                            <th className='p-4 text-right font-semibold text-sm'>
+                                Available spots
+                            </th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {events && events.length ? events.map((event) => (
+                                <tr key={event.name} className='border-b border-gray-900/10 last:border-0'>
+                                    <td className='px-6 py-3'>
+                                        <Image
+                                            className='h-10 w-10 rounded-full bg-gray-50'
+                                            src={
+                                                `${event.image}` ||
+                                                '/assets/placeholder.jpg'
+                                            }
+                                            alt='event image'
+                                            width={500}
+                                            height={500}
+                                        />
+                                    </td>
+                                    <td className='px-4 py-2 text-left text-sm font-medium'>
+                                        {event.name}
+                                    </td>
+                                    <td className='px-4 py-2 text-left text-sm'>
+                                        {event.location}
+                                    </td>
+                                    <td className='px-4 py-2 text-left text-sm'>
                                         {event.date}
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='shrink-0 flex flex-col items-end gap-2'>
-                            <button className="readmore flex items-center gap-2"><span>Read more</span><GoArrowRight className="size-4 font-semibold"/></button>
-                            <button className="error"><span>Delete event</span></button>
-                        </div>
-                    </li>
-                )) : (
+                                    </td>
+                                    <td className='px-4 py-2 text-right text-sm'>
+                                        {event.numberOfSpots}
+                                    </td>
+                                    <td className='px-4'>
+                                        <button className='readmore flex items-center gap-2'>
+                                            <span>Read more</span>
+                                            <GoArrowRight className='size-4 font-semibold' />
+                                        </button>
+                                    </td>
+                                </tr> )) : (
                     <div className='flex justify-center items-center my-2'>
                         <p className='text-secondary font-semibold'>No events found! Get started by <Link href="/admin/new" className='underline underline-offset-4 decoration-2 decoration-[var(--tertiary)] hover:decoration-[var(--secondary-muted)]'>creating an event.</Link></p>
                     </div>
                 )}
-            </ul>
-        </div>
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 };
