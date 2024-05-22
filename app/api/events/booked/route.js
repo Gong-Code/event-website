461df895136ad4ebf87e83ef08f36100d25d31a6
Devrim Akbulut <Devrimakb1@gmail.com>
2024-05-22 14:34:21 +0200

    snyggade till lite

--- app/api/events/booked/route.js
+++ app/api/events/booked/route.js
@@ -1,5 +1,3 @@
-// app/api/events/booked/route.js
-
 import { NextResponse } from 'next/server';
 import { collection, addDoc, Timestamp } from 'firebase/firestore';
 import { db } from '@/firebase.config';
@@ -7,11 +5,9 @@ import { db } from '@/firebase.config';
 export async function POST(request) {
   try {
     const { eventId, userId, userEmail } = await request.json();
-
     if (!eventId || !userId || !userEmail) {
       return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
     }
-
     const eventRef = collection(db, 'bookedEvents');
     const newBooking = {
       eventId,
@@ -19,10 +15,8 @@ export async function POST(request) {
       userEmail,
       bookedAt: Timestamp.now(),
     };
-
     const bookingRef = await addDoc(eventRef, newBooking);
     return NextResponse.json({ id: bookingRef.id, ...newBooking }, { status: 201 });
-
   } catch (err) {
     return NextResponse.json({ error: 'Error creating booking' }, { status: 500 });
   }
