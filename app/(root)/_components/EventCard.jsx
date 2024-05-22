2d51ae4cff3acd3833a65f99855cfd44f139ceb3
Emma Mellgren <mellgrenemma0@gmail.com>
2024-05-22 17:57:19 +0200

    Updated styling

--- app/(root)/_components/EventCard.jsx
+++ app/(root)/_components/EventCard.jsx
@@ -12,34 +12,48 @@ export const EventCard = ({
     eventId,
     numberOfSpots,
     userId,
-    bookedUsers
+    bookedUsers,
 }) => {
     const router = useRouter();
-    
+
     const numberOfBookedUsers = bookedUsers ? bookedUsers.length : 0;
 
     const goToEvent = () => {
-        router.push(
-            `/${eventId}`
-        );
+        router.push(`/${eventId}`);
     };
 
     return (
         <div
             onClick={goToEvent}
-            className='p-6 rounded-3xl border-dashed border-2 border-slate-600 transform transition duration-500 hover:scale-110 hover:border-tertiary cursor-pointer m-5 max-w-96 w-[150px}'>
-            <h3 className='py-4'>{name}</h3>
-            <Image
-                src={`${image}` || '/assets/placeholder.jpg'}
-                width={200}
-                height={200}
-                alt='Event image'
-            />
-            <p>{date}</p>
-            <p>{location}</p>
-            <p>
-                {numberOfBookedUsers}/{numberOfSpots}
-            </p>
+            className='p-6 bg-primary hover:bg-primary-muted hover:scale-105 rounded-md shadow-lg ring-1 ring-gray-900/5 transform transition duration-500 cursor-pointer m-5 max-w-96 w-[370px] h-[340px] text-gray-800 flex flex-col'>
+            <div className='justify-center text-left'>
+                <h3 className='mb-2 w-fit px-3 bg-tertiary font-semibold text-primary'>
+                    {name}
+                </h3>
+                <div className='w-full h-[150px]'>
+                    <Image
+                        src={`${image}` || '/assets/placeholder.jpg'}
+                        width={200}
+                        height={200}
+                        alt='Event image'
+                        className='w-full h-[150px] object-cover rounded-md'
+                    />
+                </div>
+                <div className='flex flex-col gap-2 mt-4 text-sm'>
+                    <span>
+                        <span className='font-semibold'>Location: </span>
+                        {location}
+                    </span>
+                    <span>
+                        <span className='font-semibold'>Date: </span>
+                        {date}
+                    </span>
+                    <span>
+                        <span className='font-semibold'>Booked: </span>
+                        {numberOfBookedUsers}/{numberOfSpots}
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
