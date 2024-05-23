d005612bdda6fbe856a8135bd868eeddefafa10e
Emma Mellgren <mellgrenemma0@gmail.com>
2024-05-23 16:38:57 +0200

    Removed img in list

--- app/(root)/admin/_components/EventsList.jsx
+++ app/(root)/admin/_components/EventsList.jsx
@@ -6,7 +6,6 @@ import { GoArrowRight } from 'react-icons/go';
 import { useEvents } from './events-provider';
 
 export const EventsList = () => {
-    
     const { events } = useEvents();
 
     return (
@@ -15,8 +14,7 @@ export const EventsList = () => {
                 <table className='text-slate-900 w-full table-auto'>
                     <thead className='border-b border-gray-900/10 pb-12'>
                         <tr>
-                            <th></th>
-                            <th className='p-4 text-left font-semibold text-sm'>
+                            <th className='col-span-2 p-4 text-left font-semibold text-sm'>
                                 Event name
                             </th>
                             <th className='p-4 text-left font-semibold text-sm'>
@@ -28,7 +26,6 @@ export const EventsList = () => {
                             <th className='p-4 text-right font-semibold text-sm'>
                                 Available spots
                             </th>
-                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -36,18 +33,6 @@ export const EventsList = () => {
                             <tr
                                 key={event.name}
                                 className='border-b border-gray-900/10 last:border-0'>
-                                <td className='w-[60px] invisible md:visible px-3 py-3'>
-                                    <Image
-                                        className='h-10 w-10 rounded-full bg-gray-50'
-                                        src={
-                                            `${event.image}` ||
-                                            '/assets/placeholder.jpg'
-                                        }
-                                        alt='event image'
-                                        width={40}
-                                        height={40}
-                                    />
-                                </td>
                                 <td className='px-4 py-2 text-left text-sm font-medium text-ellipsis overflow-hidden'>
                                     {event.name}
                                 </td>
