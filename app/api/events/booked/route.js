1f145616f0f407bd60a5b5333e29d58e1b587447
Devrim Akbulut <Devrimakb1@gmail.com>
2024-05-23 20:02:48 +0200

    POST function working without authentication, logging bearer token when logging in too

--- app/api/events/booked/route.js
+++ app/api/events/booked/route.js
@@ -1,29 +1,25 @@
-import { db } from "@/firebase.config";
-import { doc, updateDoc, arrayUnion} from "firebase/firestore";
+import { NextResponse } from 'next/server';
+import { arrayUnion, updateDoc, doc } from 'firebase/firestore';
+import { db } from '@/firebase.config';
 
-export async function PATCH(req, res) {
-    if (req.method === 'PATCH') {
-        try {
-            const { eventId, email, id } = req.body; // Extract eventId, email, and id from the request body
-            if (!eventId || !email || !id) {
-                throw new Error("Missing required parameters");
-            }
-            // Get reference to the event document
-            const eventRef = doc(db, 'events', eventId);
-            // Update the bookedUsers array in the event document
-            await updateDoc(eventRef, {
-                bookedUsers: firebase.firestore.FieldValue.arrayUnion({ email, id })
-            });
-
-            return new Response(JSON.stringify({ message: "User added to bookedUsers array" }), { status: 200 });
-        } catch (err) {
-            console.error("Error:", err);
-            return new Response(JSON.stringify({ error: "Failed to update bookedUsers array" }), { status: 500 });
+export async function POST(request) {
+    try {
+        const { eventId, email, id } = await request.json();
+        if (!eventId ||  !email || !id) {
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
         }
-    } else {
-        return new Response(JSON.stringify({ error: "Method Not Allowed" }), { status: 405 });
+        const eventRef = doc(db, 'events', eventId);
+        const newBooking = { email, id, bookedAt: new Date().toISOString() };
+        await updateDoc(eventRef, {
+            bookedUsers: arrayUnion(newBooking)
+        });
+        return NextResponse.json({ ...newBooking }, { status: 201 });
+    } catch (err) {
+        console.log(err.message)
+        return NextResponse.json({ error: 'Error creating booking' }, { status: 500 });
     }
 }
+
 
 
 /*{
