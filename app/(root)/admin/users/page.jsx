a5fdaa39f7b8e2f0ad378a4544350a7e357a73cf
Felicia Hu <feliciahu1992_@hotmail.com>
2024-05-23 14:41:05 +0200

    Update UsersPage to remove deprecated AdminList component and refactor useAuth hook in page.jsx and user.db.js

--- app/(root)/admin/users/page.jsx
+++ app/(root)/admin/users/page.jsx
@@ -4,19 +4,18 @@
 import withAdminAuth from "@/app/hoc/withAdminAuth";
 import { UsersList } from "../_components/UsersList";
 import { useAuth } from "../_components/auth-provider";
-import { AdminList } from "../_components/AdminList";
+
 
 
 const UsersPage = () => {
   
-  const { user, isAdmin } = useAuth();
-  
+  const { user } = useAuth();
+
   console.log(user)
   return (
     <div className="flex flex-col justify-center text-center mt-20 gap-10">
       <h1>Users</h1>
       <UsersList users={user} />
-      <AdminList admins={isAdmin} />
     </div>
   );
   
